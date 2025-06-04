65977121cdd54b585575edbcaf005c7e895a40ce
Chris :) <139085294+ChrisjL1002@users.noreply.github.com>
2025-06-03 18:07:30 -0700

    Cleaning up the awful code

--- Hangman.py
+++ Hangman.py
@@ -1,13 +1,11 @@
 # Hangman game :)
 
-# My name is Edwin.
-# I made the Mimic.
-# It was difficult to put the pieces together.
-# But unfortunately, something went so wrong, and now I can't do anything, BUT SING THIS STUPID SONG!!!!
+# Why did I make this work so weird
 
-import words
-import random
-import os
+from words import wordlist
+from random import choice
+from os import system
+# Btw I use os to exit because I don't like the python exit
 
 tries = 6
 letterguesses = 0
@@ -16,19 +14,22 @@ print("Hangman")
 print("made by ChrisjL1002 :)")
 print("")
 
-word = random.choice(words.wordlist)
+word = choice(wordlist)
 wordlen = len(word)
 
 def guess():
     global letterguesses
+    # Why do I use global SO MUCH I don't get it
     if letterguesses == wordlen:
         return input("Do you know the word yet? you guessed all the letters, word: ")
     return input("Guess: ")
 
 def guess_check(guess):
+    # Idk how tf this function works
     global letterguesses
     global word
     global tries
+    # Global hell
     positions = [i + 1 for i, letter in enumerate(word) if letter == guess]
     if guess == word:
         tries = 7
@@ -44,24 +45,20 @@ def guess_check(guess):
         return f"Letter not in word. Tries left: {tries}"
 
 def correct():
-    global word
     print("")
     print(f"You got it! The word was {word}!")
-    os.system("exit")
+    system("exit")
 
 def fail():
     print("")
-    print(f"Incorrect! The word was {word}.")
-    os.system("exit")
+    print(f"You didn't get it :( The word was {word}.")
+    system("exit")
 
 print(f"Word length: {wordlen}")
 print("")
-g = guess()
-print("")
-c = guess_check(g)
+c = "I hate python so much"
 
 while tries != 7:
-    print(c)
     if c == "Correct!":
         correct()
     elif tries == 0:
@@ -69,6 +66,8 @@ while tries != 7:
         print("")
         fail()
     else:
-        print("")
         g = guess()
         c = guess_check(g)
+        print(c)
+
+# I made this much cleaner
